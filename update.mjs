15ac2b13763fd0f57f2c1a96c0747ae8d9b45402
Thai Pangsakulyanont <dtinth@spacet.me>
2023-01-19 12:55:34 +0700

    Add repository corresponding to the DNS records

--- update.mjs
+++ update.mjs
@@ -1,7 +1,12 @@
 const config = [
+  // https://github.com/wonderfulsoftware/source.in.th/blob/main/public/_redirects
   ['source.in.th', 'CNAME', 'apex-loadbalancer.netlify.com'],
   ['www.source.in.th', 'CNAME', 'sourceinth.netlify.app'],
+
+  // https://github.com/rayriffy/lamb
   ['lamb.source.in.th', 'CNAME', 'cname.vercel-dns.com'],
+
+  // TBA
   ['open.source.in.th', 'CNAME', 'creatorsgarten.github.io'],
 ]
 
